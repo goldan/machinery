b5051c4e3d1e8295f0325975922404fcf430a832
Dennis Golomazov <dennis@trueskills.com>
2016-03-15 10:06:32 -0700

    Output experiment results in tabular format. Add diff feature.
    Change-Id: I8d46f1d0f04eaae24140a10c4cd8165dd1d69042

--- analyze_results.py
+++ analyze_results.py
@@ -3,34 +3,148 @@ u"""Print experiments results stored in MongoDB.
 
 Usage:
     analyze_results.py <db_name>
+    analyze_results.py diff <db_name> <id1> <id2>
 
 Options:
  -h --help              Show this screen.
  --version              Show Version.
  <db_name>              Mongo database name where the results are stored.
+ default action: print table with results.
+ diff: print diff between two experiments with <id1> and <id2>.
 """
 import sys
 
+from deepdiff import DeepDiff
 from docopt import docopt
 from featureforge.experimentation.stats_manager import StatsManager
+from prettytable import PrettyTable
 from tabulate import tabulate
+from machinery.common.utils import pretty_date
 
 
-def analyze_results():
-    """Print experiments results stored in MongoDB."""
-    options = docopt(__doc__)
-    manager = StatsManager(100000, options["<db_name>"])
-    headers = ['Classifier', 'scaling', 'accuracy']
-    results = [(
-        exp['classifier']['name'],
+def get_experiments(db_name, ids=None, from_time=100000):
+    """Get list of experiments from Mongo database, all or by ids.
+
+    When search by ids, prefixes are allowed, so it finds
+    all experiments which ids start from ids specified.
+
+    Args:
+        db_name: database name to connect to.
+        ids: (optional) iterable of ids to filter results by.
+        from_time: range of seconds (from now to the past),
+            to filter experiments by.
+
+    Returns:
+        list of experiments dicts.
+    """
+    manager = StatsManager(from_time, db_name)
+    return [exp for exp in manager.iter_results() if not ids or
+            any(str(exp['_id']).startswith(pk) for pk in ids)]
+
+
+def analyze_results(db_name, experiment_ids=None):
+    """Print table with experiment results stored in MongoDB.
+
+    Experiments are sorted by accuracy score reversed.
+
+    Args:
+        db_name: database name to connect to.
+        experiment_ids: (optional) iterable of ids to filter results by.
+    """
+    experiments = get_experiments(db_name, experiment_ids)
+    headers = ['ID', 'Classifier', 'Scaling', 'Grid size', 'Accuracy', 'Random state', 'Booked at']
+
+    results = sorted([(
+        exp['_id'],
+        exp['classifier']['name'].split('.')[-1],
         exp['features']['scaling'],
-        str(exp['results']['accuracy']*100) + '%') for exp in manager.iter_results()]
+        exp['results']['grid_size'],
+        str(exp['results']['accuracy']*100) + '%',
+        exp['random_state'],
+        pretty_date(exp['booked_at']))
+                      for exp in experiments], key=lambda row: row[4], reverse=True)
     print tabulate(results, headers)
+
+
+def prepare_diff_key(key):
+    """Extract deepest dict key from a DeepDiff key.
+
+    >>> prepare_diff_key("['classifier']['config']['n_neighbors']")
+    'n_neighbors'
+
+    Args:
+        key: DeepDiff (nested) key of changed dictionary value.
+
+    Returns:
+        the deepest key of the diff dictionary key.
+    """
+    return key.split('[')[-1].strip("]'")
+
+
+def prepare_diff_value(key, value):
+    """Convert DeepDiff value to a format suitable for output.
+
+    Args:
+        key: DeepDiff (nested) key of changed dictionary value.
+        value: value of the key (old or new version).
+
+    Returns:
+        value converted for suitable representation, e.g. truncated.
+    """
+    if prepare_diff_key(key) == 'git_info':
+        value = value[:70]
+    return value
+
+
+def diff_experiments(db_name, id1, id2):
+    """Print diff between experiment results stored in MongoDB.
+
+    First, print the analysis table of the two experiments.
+    Then print the diff between all the config options and results.
+    Note that id prefixes are allowed, so id search is done by prefixes.
+
+    Args:
+        db_name: database name to connect to.
+        id1: id of the first experiment to compare.
+        id2: id of the second experiment to compare.
+
+    Raises:
+        Exception if more or less than 2 experiments were found by ids.
+    """
+    analyze_results(db_name, (id1, id2))
+    experiments = get_experiments(db_name, (id1, id2))
+    if len(experiments) < 2:
+        raise Exception("No experiments with given ids")
+    if len(experiments) > 2:
+        raise Exception("Too many experiments with given ids")
+    diff = DeepDiff(*experiments)
+    table = PrettyTable()
+    table.field_names = ["Parameter", id1, id2]
+    # we make a set to eliminate duplicates in diff, e.g. 'random_state' occurs twice in keys
+    # sort by key name
+    rows = sorted(list(set([(
+        prepare_diff_key(key),
+        prepare_diff_value(key, values['oldvalue']),
+        prepare_diff_value(key, values['newvalue'])) for key, values in
+                            diff['values_changed'].items()])), key=lambda row: row[0])
+    for row in rows:
+        table.add_row(row)
+    table.align = 'l'
+    print table
+
+
+def main():
+    """Select the action from command line and execute the function."""
+    options = docopt(__doc__)
+    if options["diff"]:
+        diff_experiments(options["<db_name>"], options["<id1>"], options["<id2>"])
+    else:
+        analyze_results(options["<db_name>"])
 
 
 if __name__ == "__main__":
     try:
-        analyze_results()
+        main()
     except KeyboardInterrupt:
         pass
     sys.exit(0)
--- common/utils.py
+++ common/utils.py
@@ -1,5 +1,7 @@
 """Common utility functions and constants used throughout the project."""
 import csv
+from datetime import datetime
+
 
 CSV_OPTIONS = {'delimiter': ',', 'quotechar': '"', 'quoting': csv.QUOTE_MINIMAL}
 
@@ -26,3 +28,55 @@ def roundto(value, digits=3):
         rounded value.
     """
     return round(value, digits)
+
+
+def pretty_date(time=False):
+    """
+    Get pretty representation of a date/time.
+
+    Get a datetime object or a int() Epoch timestamp and return
+    a pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc.
+    Copied from http://stackoverflow.com/a/1551394/304209.
+
+    Args:
+        time: datetime object or epoch timestamp.
+
+    Returns:
+        date in human format, like 'an hour ago'.
+    """
+    now = datetime.now()
+    if isinstance(time, int):
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time, datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(second_diff / 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(second_diff / 3600) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        return str(day_diff / 7) + " weeks ago"
+    if day_diff < 365:
+        return str(day_diff / 30) + " months ago"
+    return str(day_diff / 365) + " years ago"
