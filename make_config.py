0dfac30785587ae3d719366ddbd54adba9d2a0bd
Dennis Golomazov <dennis@trueskills.com>
2016-03-15 10:17:26 -0700

    Add full classifiers grid parameters config
    Change-Id: Ib17809c1a0a1a9dfbbb0f8d43cf16d825c135efa

--- make_config.py
+++ make_config.py
@@ -17,12 +17,100 @@ import sys
 from collections import OrderedDict
 from hashlib import sha256
 from itertools import product
+from numpy import linspace
 import pandas
 
 from docopt import docopt
 
 
-def make_config():
+def classifiers_config(random_state):
+    """Get classifiers config dict with parameter grid.
+
+    Classifier config dict values can be iterables, meaning
+    that a grid search should be performed.
+    If they are not iterables (e.g. random_state is int),
+    the values are just to be passed to a classifier constructor.
+
+    Args:
+        random_state: random_state config option to include to some classifiers.
+
+    Returns:
+        dict {<classifier_name>: {<classifier_config_dict>}}.
+    """
+    return {
+        'tree.DecisionTreeClassifier': {
+            'criterion': ('gini', 'entropy'),
+            'splitter': ('best', 'random'),
+            'max_features': ('auto', 'sqrt', 'log2', None),
+            'class_weight': ('balanced', None),
+            'random_state': random_state
+        },
+        'neighbors.KNeighborsClassifier': {
+            'n_neighbors': range(1, 101, 3),
+            'weights': ('uniform', 'distance'),
+            'algorithm': ('auto', 'ball_tree', 'kd_tree', 'brute'),
+            'p': range(1, 11)
+        },
+        'svm.LinearSVC': {
+            'C': range(11),
+            'loss': ('hinge', 'squared_hinge'),
+            'penalty': ('l1', 'l2'),
+            'dual': (True, False),
+            'multi_class': ('ovr', 'crammer_singer'),
+            'class_weight': ('balanced', None),
+            'random_state': random_state
+        },
+        'svm.SVC': {
+            'C': range(11),
+            'kernel': ('linear', 'poly', 'rbf', 'sigmoid', 'precomputed'),
+            'shrinking': (True, False),
+            'class_weight': ('balanced', None),
+            'decision_function_shape': ('ovo', 'ovr'),
+            'random_state': random_state
+        },
+        'ensemble.RandomForestClassifier': {
+            'n_estimators': range(2, 50, 4),
+            'criterion': ('gini', 'entropy'),
+            'max_features': ('auto', 'sqrt', 'log2', None),
+            'bootstrap': (True, False),
+            'oob_score': (True, False),
+            'random_state': random_state,
+            'class_weight': ('balanced', 'balanced_subsample', None),
+        },
+        'ensemble.ExtraTreesClassifier': {
+            'n_estimators': range(2, 50, 4),
+            'criterion': ('gini', 'entropy'),
+            'max_features': ('auto', 'sqrt', 'log2', None),
+            'bootstrap': (True, False),
+            'oob_score': (True, False),
+            'random_state': random_state,
+            'class_weight': ('balanced', 'balanced_subsample', None),
+        },
+        'ensemble.AdaBoostClassifier': {
+            'n_estimators': range(10, 101, 10),
+            'learning_rate': [1] + list(linspace(0.1, 10, 21)),
+            'algorithm': ('SAMME', 'SAMME.R'),
+            'random_state': random_state
+        },
+        'ensemble.GradientBoostingClassifier': {
+            'loss': ('deviance', 'exponential'),
+            'learning_rate': [0.1] + list(linspace(0.01, 3, 21)),
+            'n_estimators': range(50, 301, 25),
+            'max_depth': range(1, 11),
+            'subsample': [1] + list(linspace(0.1, 3, 21)),
+            'max_features': ('auto', 'sqrt', 'log2', None),
+            'random_state': random_state
+        },
+        'naive_bayes.GaussianNB': {},
+        'discriminant_analysis.LinearDiscriminantAnalysis': {
+            'solver': ('svd', 'lsqr', 'eigen'),
+            'shrinkage': (None, 'auto'),
+        },
+        'discriminant_analysis.QuadraticDiscriminantAnalysis': {},
+    }
+
+
+def make_config(options):
     """Make json configuration file for classification.
 
     Most parameters are the same and are taken from the command-line arguments,
@@ -30,27 +118,26 @@ def make_config():
     Some parameters are hard-coded (e.g. class names).
     And some are hard-coded and are cycled by (scaling, classifier names).
     So that every combination of parameters is written into config file.
+
+    Args:
+        options: CLI options dict.
     """
-    options = docopt(__doc__)
     random_state = random.randint(0, 1000000)
     class_names = ('0 Insuff', '1 Junior', '2 Exp-ed', '3 Expert')
-    classifiers = ['tree.DecisionTreeClassifier', 'svm.LinearSVC']
     feature_scaling_options = (False, True)
-    verbose = False
     data = pandas.read_csv(options["<features.csv>"])
     classes = pandas.read_csv(options["<classes.csv>"])
-    # class_code: number of examples, e.g. {0: 10, 1: 20, 2: 35, 3: 12}
+    # class_code: number of examples per class, e.g. {0: 10, 1: 20, 2: 35, 3: 12}
     classes_counts_raw = dict(classes[classes.columns[0]].value_counts())
     # class name: number of examples
     classes_counts = [(name, classes_counts_raw[i]) for i, name in enumerate(class_names)]
+    classifiers = classifiers_config(random_state)
     config = []
     for classifier, scaling in product(classifiers, feature_scaling_options):
         dct = OrderedDict()
         dct["classifier"] = OrderedDict((
             ("name", classifier),
-            ("config", {
-                "random_state": random_state
-            })))
+            ("config", classifiers[classifier])))
         dct["classes"] = OrderedDict((
             ("names", classes_counts),
             ("total", sum(classes_counts_raw.values())),
@@ -62,7 +149,8 @@ def make_config():
             ("filename", options["<features.csv>"]),
             ("filehash", sha256(open(options["<features.csv>"], 'rb').read()).hexdigest()),
             ("names", sorted(data.columns))))
-        dct["verbose"] = verbose
+        dct["verbose"] = False
+        dct["random_state"] = random_state
         config.append(dct)
 
     with open(options["<config.json>"], "w") as fout:
@@ -71,7 +159,7 @@ def make_config():
 
 if __name__ == "__main__":
     try:
-        make_config()
+        make_config(docopt(__doc__))
     except KeyboardInterrupt:
         pass
     sys.exit(0)
